3b80733bcb7039c112af5fe7c108e0dc913387d0
Clément Vidal <clementvidalperso@gmail.com>
2015-12-17 17:12:31 +0100

    Add some css utilities class and remove auto paragraph

--- functions.php
+++ functions.php
@@ -1,9 +1,17 @@
 <?php
+
+
+// Allow svg upload
 function cc_mime_types($mimes) {
   $mimes['svg'] = 'image/svg+xml';
   return $mimes;
 }
+
 add_filter('upload_mimes', 'cc_mime_types');
+// disable filter that automatcly add p after each double line break
+remove_filter( 'the_content', 'wpautop' );
+remove_filter( 'the_excerpt', 'wpautop' );
+
 add_theme_support( 'post-thumbnails', array( 'skill', 'partner', 'portfolio_entry' ) );
 require get_template_directory() . '/inc/functions/bootstrap_menuwalker.php';
 require get_template_directory() . '/inc/functions/enqueue_scripts.php';
