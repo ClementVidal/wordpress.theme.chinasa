929736f8fc3d8bcfc576a3b815f899c6c91edafd
Clément Vidal <clementvidalperso@gmail.com>
2016-02-10 10:27:16 +0100

    Fix d'un du problème lié a l'execution de code avant le chargement total de la page

--- js/dist/chinasa.js
+++ js/dist/chinasa.js
@@ -1,23 +1,23 @@
 var chinasa = angular.module('chinasa', ['duScroll', 'ngCookies', 'ngTouch']);
-chinasa.controller( 'bodyController', chiBodyController );
+chinasa.controller('bodyController', chiBodyController);
 
 var attachSidebarFunction = function($scope, $rootScope, $document) {
     $scope.sidebarVisible = false;
 
     $scope.showSidebar = function() {
-        if( !$scope.sidebarVisible ) {
+        if (!$scope.sidebarVisible) {
             $scope.sidebarVisible = true;
         }
     };
 
     $scope.hideSidebar = function() {
-        if( $scope.sidebarVisible ) {
+        if ($scope.sidebarVisible) {
             $scope.sidebarVisible = false;
         }
     };
 
     $scope.toggleSidebar = function() {
-        if( $scope.sidebarVisible ) {
+        if ($scope.sidebarVisible) {
             $scope.sidebarVisible = false;
         } else {
             $scope.sidebarVisible = true;
@@ -26,8 +26,8 @@ var attachSidebarFunction = function($scope, $rootScope, $document) {
 
     // Setup event handler to handle click in the document or press on escape
     // This is catched and hide the sidebar
-    $rootScope.$on("documentClicked", _close);
-    $rootScope.$on("escapePressed", _close);
+    $rootScope.$on('documentClicked', _close);
+    $rootScope.$on('escapePressed', _close);
 
     function _close() {
         $scope.$apply(function() {
@@ -35,29 +35,33 @@ var attachSidebarFunction = function($scope, $rootScope, $document) {
         });
     }
 
-    $document[0].addEventListener("keyup", function(e) {
-        if (e.keyCode === 27)
-            $rootScope.$broadcast("escapePressed", e.target);
+    $document[0].addEventListener('keyup', function(e) {
+        if (e.keyCode === 27) {
+            $rootScope.$broadcast('escapePressed', e.target);
+        }
     });
 
-    $document[0].addEventListener("click", function(e) {
-        $rootScope.$broadcast("documentClicked", e.target);
+    $document[0].addEventListener('click', function(e) {
+        $rootScope.$broadcast('documentClicked', e.target);
     });
 };
 
+chiBodyController.$inject = ['$scope', '$rootScope', '$document', '$cookies', '$timeout'];
 
-chiBodyController.$inject = ['$scope', '$rootScope', '$document', '$cookies' ];
-function chiBodyController( $scope, $rootScope, $document, $cookies ){
+function chiBodyController($scope, $rootScope, $document, $cookies, $timeout) {
 
-    // Play CSS animation if necessary based on cookie.
-    if( ! $cookies.get( 'animAlreadyPlayed' ) ) {
-        $cookies.put( 'animAlreadyPlayed', 'true' );
-        $('#chi-sidebar').get(0).classList.add( 'chi-enlarge-anim' );
-        $('.chi-sidebar-footer').get(0).classList.add( 'chi-footer-up-and-bounce-anim' );
-        $('.chi-sidebar-logo').get(0).classList.add( 'chi-logo-up-and-bounce-anim' );
-    }
+    $timeout(function() {
 
-    attachSidebarFunction($scope, $rootScope, $document);
+        // Play CSS animation if necessary based on cookie.
+        if (!$cookies.get('animAlreadyPlayed')) {
+            $cookies.put('animAlreadyPlayed', 'true');
+            $('#chi-sidebar').get(0).classList.add('chi-enlarge-anim');
+            $('.chi-sidebar-footer').get(0).classList.add('chi-footer-up-and-bounce-anim');
+            $('.chi-sidebar-logo').get(0).classList.add('chi-logo-up-and-bounce-anim');
+        }
+
+        attachSidebarFunction($scope, $rootScope, $document);
+    });
 }
 ;(function() {
     'use strict';
